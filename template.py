1f6a65b44327252fdec2c720d46c213c7bca81d5
dgWelk <lexesusvinok@gmail.com>
2026-03-23 17:10:24 +0300

    Add files via upload

---
+++ template.py
@@ -0,0 +1,15 @@
+def start(item, name, target, statement):
+    print(
+        f'''
+ПЗ №{item}, Винокуров А. В., ИБА-22
+{name},
+Цель: {target}
+Постановка задачи: {statement}''')
+
+
+
+def main():
+    pass
+
+
+main()
